e513ec601a24c9c5cdde2d795c9aa1065d83c590
Elias <elias.alhomsi@mail.mcgill.ca>
2018-04-05 16:32:07 -0400

    fixing things

---
+++ EventRegistration-Spring/src/main/java/ca/mcgill/ecse321/eventregistration/dto/ParticipantDto.java
@@ -0,0 +1,43 @@
+package ca.mcgill.ecse321.eventregistration.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.mcgill.ecse321.eventregistration.model.Participant;
+import ca.mcgill.ecse321.eventregistration.model.Registration;
+import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
+
+public class ParticipantDto {
+	private String name;
+	private List<EventDto> events;
+
+	public ParticipantDto() {
+	}
+
+	public ParticipantDto(String name) {
+		this(name, new ArrayList<EventDto>());
+	}
+
+	public ParticipantDto(String name, ArrayList<EventDto> arrayList) {
+		this.name = name;
+		this.events = arrayList;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<EventDto> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<EventDto> events) {
+		this.events = events;
+	}
+
+}
